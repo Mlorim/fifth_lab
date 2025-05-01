456ad336fa9d767b31d1a4557c4bf89aa448725a
Pomazkov Miron <111051727+Mlorim@users.noreply.github.com>
2025-05-01 13:04:39 +0300

    Update 1.1.cpp

--- 1.1.cpp
+++ 1.1.cpp
@@ -95,7 +95,7 @@ int main() {
     std::cout << std::endl;
 
     // List => error
-    # ifdef False
+    # if 0
     std::cout << "List:" << std::endl;
     std::list<int> lst;
     for (int i = 0; i < size; ++i) {
